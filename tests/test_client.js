4f64a4e7fed29ebe5945cf6b3a3ffbf2d4b21526
Grzegorz Tanczyk <fb@tanczyk.pl>
2013-08-11 20:41:10 +0200

    client test

--- tests/test_client.js
+++ tests/test_client.js
@@ -1,2 +1,20 @@
-class Test {
-}
+// test_client.js
+
+require(['World:../lib/world.js', 'Player:../lib/player.js', 'Viewport:../lib/viewport.js'], //
+	function(World, Player, Viewport) {
+		var world = new World();
+
+		var playerOne = new Player();
+		var playerTwo = new Player();
+
+		world.addPlayer(playerOne);
+		world.addPlayer(playerTwo);
+
+		var view = new Viewport(document.querySelector("#viewport"));
+
+		view.animation(function(dt) {
+			view.circle(0, 0, 100);
+		});
+		
+		view.init();
+	});
