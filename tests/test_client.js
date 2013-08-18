bfe2725445fba39a51f13f8ee97c859111b57b57
Grzegorz Tanczyk <fb@tanczyk.pl>
2013-08-18 14:26:49 +0200

    asset loading module

--- tests/test_client.js
+++ tests/test_client.js
@@ -1,25 +1,29 @@
 // test_client.js
 
-require(['World:lib/world.js', 'Player:lib/player.js', 'Viewport:lib/viewport.js'], //
-	function(World, Player, Viewport) {
+require(['World:lib/world.js', 'Player:lib/player.js', 'Viewport:lib/viewport.js', 'Assets:lib/gamedev-lib/lib/gfx/assets.js'], //
+	function(World, Player, Viewport, Assets) {
+		var assets = new Assets('../assets/');
+
 		var world = new World();
 
 		var playerOne = new Player('player 1');
 
 		var PlayerTwo = Player.extend(function PlayerTwo(name) {
-				this.superclass.Player.call(this, name);
-			});
-		
+			this.superclass.Player.call(this, name);
+		}
+		);
+
 		var playerTwo = new PlayerTwo('player 2');
 
 		world.addPlayer(playerOne);
 		world.addPlayer(playerTwo);
 
 		var view = new Viewport(document.querySelector("#viewport"));
-
-		view.animation(function(dt) {
-			view.circle(0, 0, 100);
-		});
-
 		view.init();
+
+		assets.loadImages(['sprites/liukang/gdsg.png']).then(function(sprites) {
+			view.animation(function(dt) {
+				view.circle(0, 0, 100, sprites[0]);
+			});
+		});
 	});
