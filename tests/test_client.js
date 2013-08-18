71c1bad134bececebf858863f727de93ce20f78a
Grzegorz Tanczyk <fb@tanczyk.pl>
2013-08-18 13:21:22 +0200

    BaseClass inheritance module

--- tests/test_client.js
+++ tests/test_client.js
@@ -4,8 +4,13 @@ require(['World:lib/world.js', 'Player:lib/player.js', 'Viewport:lib/viewport.js
 	function(World, Player, Viewport) {
 		var world = new World();
 
-		var playerOne = new Player();
-		var playerTwo = new Player();
+		var playerOne = new Player('player 1');
+
+		var PlayerTwo = Player.extend(function PlayerTwo(name) {
+				this.superclass.Player.call(this, name);
+			});
+		
+		var playerTwo = new PlayerTwo('player 2');
 
 		world.addPlayer(playerOne);
 		world.addPlayer(playerTwo);
