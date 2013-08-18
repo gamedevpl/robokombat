d6a9efa02affa3c876347580f0af7cd391278a11
Grzegorz Tanczyk <fb@tanczyk.pl>
2013-08-18 19:05:57 +0200

    - Simple sprite animation i client test - Viewport transform API

--- tests/test_client.js
+++ tests/test_client.js
@@ -1,7 +1,8 @@
 // test_client.js
 
-require(['World:lib/world.js', 'Player:lib/player.js', 'Viewport:lib/viewport.js', 'Assets:lib/gamedev-lib/lib/gfx/assets.js'], //
-	function(World, Player, Viewport, Assets) {
+require(['World:lib/world.js', 'Player:lib/player.js', 'Viewport:lib/viewport.js',
+	'Assets:lib/gamedev-lib/lib/gfx/assets.js', 'Sprite:lib/gamedev-lib/lib/gfx/sprite.js'], //
+	function(World, Player, Viewport, Assets, Sprite) {
 		var assets = new Assets('../assets/');
 
 		var world = new World();
@@ -10,8 +11,7 @@ require(['World:lib/world.js', 'Player:lib/player.js', 'Viewport:lib/viewport.js
 
 		var PlayerTwo = Player.extend(function PlayerTwo(name) {
 			this.superclass.Player.call(this, name);
-		}
-		);
+		});
 
 		var playerTwo = new PlayerTwo('player 2');
 
@@ -21,9 +21,16 @@ require(['World:lib/world.js', 'Player:lib/player.js', 'Viewport:lib/viewport.js
 		var view = new Viewport(document.querySelector("#viewport"));
 		view.init();
 
-		assets.loadImages(['sprites/liukang/gdsg.png']).then(function(sprites) {
+		assets.loadImages(['sprites/liukang/idle.png']).then(function(sprites) {
+			var sprite = new Sprite(sprites[0], { spriteWidth: 77, spriteHeight: 133 });
+
+			var t = 0;
 			view.animation(function(dt) {
-				view.circle(0, 0, 100, sprites[0]);
+				view.clear('#000000');
+				view.transform().translate(-77/2, -133/2).then(function() {
+					sprite.index = ((t += dt*10) >> 0) % sprite.frameCount;
+					sprite.render(view.getContext());
+				});
 			});
 		});
 	});
